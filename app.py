92e3a3666c4e29609b0045f923febded1379aaa8
JauniKappauni <social.jauni@gmail.com>
2025-02-08 00:32:42 +0100

    Added error handling Added output of specific error

--- app.py
+++ app.py
@@ -8,16 +8,21 @@ def hello_world():
     output = ""
     if request.method == "POST":
         host = request.form.get("host")
-        port = request.form.get("port") 
-        username = request.form.get("username") 
-        password = request.form.get("password") 
-        command = request.form.get("command") 
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(host,port=port,username=username,password=password)
-        _stdin, _stdout, _stderr = client.exec_command(command)
-        output = _stdout.read().decode() + _stderr.read().decode()
-        client.close()
+        port = request.form.get("port")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        command = request.form.get("command")
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(host,port=port,username=username,password=password)
+
+            _stdin, _stdout, _stderr = client.exec_command(command)
+            output = _stdout.read().decode() + _stderr.read().decode()
+
+            client.close()
+        except Exception as e:
+            output = f"Fehler: {str(e)}"
     return render_template("index.html",output=output)
 
 if __name__ == "__main__":
